dec298dc539e3899fd692a10a2f82da3702fe8e6
Pragadheeswaran <36662376+Pragadheeswaran@users.noreply.github.com>
2018-02-20 19:45:08 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,8 +1,10 @@
-public class CaughtSemantic
+public class Average
 {
 public static void main(String[] args)
 {
-int myInt;
-System.out.println(myInt);
+float sum = 95.0 + 80.5 + 87.5;
+int count = 3;
+float avg = sum / count;
+System.out.println(avg);
 }
 }
